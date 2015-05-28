6fce0d8efe11bc743084696651d393a3b78a2ea3
Matt Denny <matthewjdenny@gmail.com>
2015-05-28 12:12:57 -0400

    updated example code

--- Scripts/Day_Two_Exercise.R
+++ Scripts/Day_Two_Exercise.R
@@ -1,37 +1,66 @@
-#Exercise 2-- in this exercise you will be going for speed and clarity. You will be taking the block of code below and wrapping it into several functions and parallelizing everything to make it run faster. This assignment will be more similar to what you might actually encounter in the wild
+#' Exercise 2-- in this exercise you will be going for speed and clarity. You 
+#' will be taking the block of code below and wrapping it into several functions
+#' and parallelizing everything to make it run faster. This assignment will be
+#' more similar to what you might actually encounter in the wild. (notice how I 
+#' use the "#'" symbol at the beginning of each line? In RStudio this will 
+#' automatically continue the block comment --- super handy!)
 
-#some preliminaries
-rm(list = ls())
 
-#you will need to set this to the location of th folder you downloaded for this workshop
-setwd("~/Dropbox/RA_and_Consulting_Work/ISSR_Consulting_Work/Advanced_Data_Management_in_R")
+#' Here is the start of the code you will be modifying. It reads in 11 years of
+#' cosponosrship data and turns it into sociomatricies suitable for network
+#' analysis. You will want to make use of parts of this block of code -- with 
+#' heavy modifications -- in the exercise
 
-#We are going to be looking at a dataset that comprises 11 sessions on congress and sepcifically the bills cosponsored by senators over that period of time. This is a very complex data management problem.
+############################ START EXAMPLE CODE ################################
+
+# set working directory
+mywd <- "~/Dropbox/RA_and_Consulting_Work/ISSR_Data_Science_Summer_Summit_15/Data"
+setwd(mywd)
+
+#' We are going to be looking at a dataset that comprises 11 sessions on 
+#' congress and sepcifically the bills cosponsored by senators over that period
+#' of time. This is a very complex data management problem.
 Congresses <- 11
+Bills_To_Use <- 100
 
-#1. The assign function lets us name objects usign the paste function in R. This is good for batch reading in data or generating lots of data objects.
+#' The assign() function lets us name objects usign the paste function in R. 
+#' This is good for batch reading in data or generating lots of data objects.
 
-#lets begin by loading in some data:
+# lets begin by loading in some data:
 print("Loading Raw Senate Cosponsorship Matrix Data")
 for(i in 1:Congresses){
     cur <- 99 + i
-    temp <- read.csv(paste(cur,"_senmatrix.txt", sep = ""), stringsAsFactors = F, header = F)
-    #we are only going to look at the first 100 bills from each congress
-    temp <- temp[,1:100]
-    assign(paste("Senate_Raw_Cosponsorship_",cur, sep = ""),temp) #assigns the raw cosponsorship data to a named variable
+    
+    # read in the .csv files
+    data_name <- paste(cur,"_senmatrix.txt", sep = "")
+    temp <- read.csv(data_name, stringsAsFactors = F, header = F)
+    
+    # keep first Bills_To_Use bills from each congress
+    temp <- temp[,1:Bills_To_Use]
+    
+    #name th object
+    object_name <-  paste("Senate_Raw_Cosponsorship_",cur, sep = "")
+    
+    #assigns the raw cosponsorship data to a named variable
+    assign(object_name,temp) 
 }
 
 
-#2. The get() function performs the opposite funtion of the assign function and lets us get objects by their pasted name:
+#' The get() function performs the opposite funtion of the assign function and 
+#' lets us get objects by their pasted name:
 
-#get a list of all the raw data which we will not care about after we have transformed it so we can remove it later
+#' save a list of all the raw data which we will not care about after we have 
+#' transformed it so we can remove it later
 all_objects <- ls()
 
-print("Transforming Raw data into Cosponsorship Matricies")
+cat("Transforming Raw data into Cosponsorship Matricies \n")
 for(i in 1:Congresses){
     cur <- 99 + i
-    print(paste("Currently on Congress number: ",cur, sep = ""))
-    temp <- get(paste("Senate_Raw_Cosponsorship_",cur, sep = ""))
+    cat(paste("Currently on Congress number: ",cur,"\n", sep = ""))
+    
+    # get the current data object
+    object_name <-  paste("Senate_Raw_Cosponsorship_",cur, sep = "")
+    temp <- get(object_name)
     
     #create a sociomatrix to populate
     num_senators <- length(temp[,1])
@@ -55,16 +84,19 @@ for(i in 1:Congresses){
         }
     }
     
-    assign(paste("Senate_Cosponsorship_Matrix_",cur, sep = ""),temp_sociomatrix) #assigns the cosponsorship matrix to a named variable
+    sociomat_name <- paste("Senate_Cosponsorship_Matrix_",cur, sep = "")
+    # assigns the cosponsorship matrix to a named variable
+    assign(sociomat_name,temp_sociomatrix) 
    
 }
 
-#remove all of the raw data
+#' remove all of the raw data -- how could we do all of this without creating 
+#' these objects in the first place?
 rm(list = all_objects)
 
-
-#3. define a function 
-#so far we have written code which could be turned into a function. A simple example might be a function that counts the total number of cosponsorships in a given congress.
+#' so far we have written code which could be turned into a function. A simple
+#' example might be a function that counts the total number of cosponsorships in
+#' a given congress.
 
 Total_Cosponsorships <- function(cosponsorship_matrix){
     total <- 0
@@ -76,50 +108,37 @@ Total_Cosponsorships <- function(cosponsorship_matrix){
     return(total)
 }
 
-#now we have to enter the function in the console before we can use it. If you are using RStudio then you will see the function pop up in your workspace.
-#try it out!
+#' now we have to enter the function in the console before we can use it. If you
+#' are using RStudio then you will see the function pop up in your workspace.
+#' try it out!
 Total_Cosponsorships(Senate_Cosponsorship_Matrix_100)
 
-#4. We can use functions inside of functions as well. Note that we can set default values for a function by putting an = sign next to the arguemnt name
+#' We can use functions inside of functions as well. Note that we can set 
+#' default values for a function by putting an = sign next to the arguemnt name
 
 Cosponsorships_per_Congress <- function(start = 100, end = 110){
     num_cases <- end - start + 1
     vector <- rep(0, times = num_cases)
     for(i in 1:num_cases){
         cur <- start - 1 + i
-        #sometimes we want an update of where we are in the process
-        print(paste("Currently on Congress Number: ", cur , sep = ""))
-        #we can also call a function from inside a function... inside a function... inside a function
+        #' sometimes we want an update of where we are in the process
+        cat(paste("Currently on Congress Number: ", cur ,"\n", sep = ""))
+        #' we can also call a function from inside a function... inside a 
+        #' function... inside a function
         vector[i] <- Total_Cosponsorships(get(paste("Senate_Cosponsorship_Matrix_",cur, sep = "")))
     }
     return(vector)
 }
 
-#try it out (note that you do not need to have a )
+#' try it out (note that you do not need to have any arguments because we have 
+#' set default values for all arguments -- this can be dangerous!)
 Cosp_per_Congress <- Cosponsorships_per_Congress()
 
 #plot our work
 barplot(Cosp_per_Congress,xlab = "Congress", ylab = "Number of Cosponsorships", names = c(100:110),col = rainbow(11))
 
 
-
-#5. try loading in our functions from a source file
-source("My_Functions.R")
-
-#test them out
-Threshold_100 <- Threshold(1,Senate_Cosponsorship_Matrix_100)
-RowSums_100 <- Row_Sums(Senate_Cosponsorship_Matrix_100)
-
-
-#6. The apply family of functions
-
-# we can get the rowsums using the apply function
-RowSums_Apply_100 <- apply(Senate_Cosponsorship_Matrix_100,1, sum) #takes row sums
-ColSums_Apply_100 <- apply(Senate_Cosponsorship_Matrix_100,2, sum) #takes column sums
-Mat_Plus_10 <- apply(Senate_Cosponsorship_Matrix_100,1, AddTen) #adds ten to every element of the matrix
-Mat_Plus_10 <- sapply(Senate_Cosponsorship_Matrix_100, AddTen) #adds ten to every element of the matrix
-
-#6.5 having some fun
+#6.5 having some fun -- what does this function do? How does it work?
 library(statnet)
 
 par(mfrow = c(1,1))
@@ -142,29 +161,6 @@ result <- mapply(netplot, years,colors)
 
 apply(result,sum)
 
-#7. an example with the reshape package
-library(reshape)
-
-#generate a toy dataset quality
-a <- c(1:100)
-b <- rep(c(1:4),25)
-c <- rep(c(1:10),10)
-d <- runif(100,1,100)
-e <- seq(from = 1.01, to = 2, by = 0.01)
-
-data <- as.data.frame(cbind(a,b,c,d,e))
-names(data) <- c("id","color","age","taste","quality")
-
-meltdat <- melt(data, id = c("age","color"))
-
-# cast(data, formula, function) 
-cast(meltdat, color~variable, mean)
-cast(meltdat, age~variable, sum)
-cast(meltdat, age~variable, AddTen)
-cast(meltdat, age + color~variable, mean)
-
-
-
-
+############################ END EXAMPLE CODE ##################################
 
 
