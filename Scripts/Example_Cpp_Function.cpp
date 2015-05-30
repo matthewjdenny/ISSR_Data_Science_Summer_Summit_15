814b0868dadb69669f8b98ad58be26db62c88d32
Matt Denny <matthewjdenny@gmail.com>
2015-05-30 15:12:10 -0400

    changed example C++ function

--- Scripts/Example_Cpp_Function.cpp
+++ Scripts/Example_Cpp_Function.cpp
@@ -1,26 +1,21 @@
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
+//[[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
-
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
-// [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+//[[Rcpp::export]]
+List My_Function(
+    double my_double,
+    int some_number,
+    arma::vec a_vector,
+    arma::mat example_matrix,
+    NumericVector array,
+    List some_vectors,
+){
+  IntegerVector dim = array.attr("dim"); 
+  arma::cube my_array(array.begin(),dim[0], dim[1], dim[2], false);
+  
+  double new_double = 100*my_double;
+  
+  List to_return(1);
+  to_return[0] = new_double;
+  return to_return;
 }
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
